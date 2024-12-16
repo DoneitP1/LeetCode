03f9481184254745214dd88de3e2911e37b75a58
doneitp1 aka ee29 <145833591+DoneitP1@users.noreply.github.com>
2024-12-16 15:08:49 +0300

    Time: 0 ms (100%), Space: 7.7 MB (60.24%) - LeetHub

--- 2507-number-of-common-factors/2507-number-of-common-factors.c
+++ 2507-number-of-common-factors/2507-number-of-common-factors.c
@@ -1,10 +1,8 @@
-int commonFactors(int a, int b)
-{
-    int e=0;
-    for(int i=1;i<=(a>b?b:a);i++)
-    {
-        if(a%i==0 && b%i==0)
-        e++;
+int commonFactors(int a, int b) {
+    int count=0;
+    for(int i=1; i<= fmin(a, b); i++){
+        if(a%i==0 && b%i ==0)
+        count++;
     }
-    return e;
+    return count;
 }
