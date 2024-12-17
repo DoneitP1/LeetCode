c61415f535db3e0cba5750639b86843f7e3b8ed3
doneitp1 aka ee29 <145833591+DoneitP1@users.noreply.github.com>
2024-12-17 09:40:26 +0300

    Time: 3 ms (100%), Space: 13.1 MB (100%) - LeetHub

---
+++ 2300-construct-string-with-repeat-limit/2300-construct-string-with-repeat-limit.c
@@ -0,0 +1,29 @@
+char* repeatLimitedString(char* s, int repeatLimit) {
+  int hash[26] = {0}, size = 0;
+  while (s[size]) {
+    ++hash['z' - s[size]];
+    ++size;
+  }
+  char* temp = s;
+  for (int i = 0; i < 26; ++i) {
+    while (hash[i]) {
+      *temp = 'z' - i;
+      ++temp;
+      --hash[i];
+    }
+  }
+  int repeated = 0, next = 0;
+  for (int i = 1; i < size; ++i) {
+    // keeps track of number of letters repeated
+    (s[i - 1] == s[i]) ? (++repeated) : (repeated = 0);
+    if (repeated == repeatLimit) {
+      // finds the next character that is less than the current charcter
+      while (next < size && s[i] <= s[next]) ++next;
+      // swaps characters
+      char temp = s[i];
+      s[i] = s[next];
+      s[next] = temp;
+    }
+  }
+  return s;
+}
