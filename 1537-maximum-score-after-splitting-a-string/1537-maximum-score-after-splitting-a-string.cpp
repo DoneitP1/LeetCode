59558cdfd244fcd03446a38687d5eeeae1977f8b
doneitp1 aka ee29 <145833591+DoneitP1@users.noreply.github.com>
2025-01-01 18:28:40 +0300

    Time: 2 ms (24.89%), Space: 8.8 MB (23.35%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     int maxScore(string s) {
-        // Count total number of 1's in the string
         int totalOnes = 0;
         for (char c : s) {
             if (c == '1') totalOnes++;
         }
 
-        int leftZeroes = 0;  // Number of zeroes in the left part
-        int rightOnes = totalOnes;  // Number of ones in the right part (initially all the ones)
-        int maxScore = 0;  // To store the maximum score
-
-        // Iterate through the string and check score at each position
-        for (int i = 0; i < s.length() - 1; i++) {  // We don't consider the last split as it leaves the right part empty
+        int leftZeroes = 0;  
+        int rightOnes = totalOnes;  
+        int maxScore = 0;  
+        for (int i = 0; i < s.length() - 1; i++) {  
             if (s[i] == '0') {
-                leftZeroes++;  // Increment leftZeroes if it's a 0
+                leftZeroes++;  
             } else {
-                rightOnes--;  // Decrement rightOnes if it's a 1
+                rightOnes--;  
             }
 
-            // Update the maximum score
+            
             maxScore = max(maxScore, leftZeroes + rightOnes);
         }
 
