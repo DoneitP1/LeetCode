cca7c0392bd18ae1bd3ac865f04076c18bdc8bb7
doneitp1 aka Emirhan <145833591+DoneitP1@users.noreply.github.com>
2025-03-12 12:32:56 +0300

    Time: 0 ms (100%), Space: 11.2 MB (88.07%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.c
@@ -0,0 +1,14 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int maxDepth(struct TreeNode* root) {
+    if (!root) return 0;
+    int maxLeft = maxDepth(root->left);
+    int maxRight = maxDepth(root->right);
+    return fmax(maxLeft, maxRight) + 1;
+}
